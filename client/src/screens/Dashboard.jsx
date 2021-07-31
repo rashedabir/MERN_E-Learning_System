93f158775855544e54dd4dd67b95ea4abc2aaa51
rashedabir <rashed15-2155@diu.edu.bd>
2021-07-31 11:13:25 +0600

    make responsive

--- client/src/screens/Dashboard.jsx
+++ client/src/screens/Dashboard.jsx
@@ -53,6 +53,7 @@ function Dashboard() {
               Learn How To Build Websites & Apps Write A Code Or Start A
               Business
             </Typography>
+            <br />
             <Button variant="contained" color="primary">
               make a tour
             </Button>
